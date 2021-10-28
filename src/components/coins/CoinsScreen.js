e7a0a145b01cbd18b1258ebd4fde5d2cbc646070
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2021-10-27 22:56:58 -0300

    style in detail

--- src/components/coins/CoinsScreen.js
+++ src/components/coins/CoinsScreen.js
@@ -1,14 +1,42 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Pressable, StyleSheet} from 'react-native';
 
 class CoinsScreen extends Component {
+  handlerPress = () => {
+    console.log('Press Handler');
+  };
+
   render() {
     return (
-      <View>
-        <Text>Coins Screen</Text>
+      <View style={styles.container}>
+        <Text style={styles.titleText}>Coins Screen</Text>
+        <Pressable style={styles.btn} onPress={this.handlerPress}>
+          <Text style={styles.btnText}>Ir a Deteail</Text>
+        </Pressable>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+  titleText: {
+    color: '#fff',
+    textAlign: 'center',
+  },
+  btn: {
+    padding: 8,
+    backgroundColor: 'blue',
+    borderRadius: 8,
+    margin: 16,
+  },
+  btnText: {
+    color: '#fff',
+    textAlign: 'center',
+  },
+});
 
 export default CoinsScreen;
