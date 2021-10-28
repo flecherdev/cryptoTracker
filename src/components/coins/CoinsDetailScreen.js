2076d14efc94f2782da0ee8fbe3e58c4c4069f09
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2021-10-27 22:56:05 -0300

    added screen for detail

---
+++ src/components/coins/CoinsDetailScreen.js
@@ -0,0 +1,14 @@
+import Reat, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+class CoinsDetailScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Coin Detail Screen</Text>
+      </View>
+    );
+  }
+}
+
+export default CoinsDetailScreen;
