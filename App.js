b382a445f97d661d88e62347ea6f1db54b1f96a1
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2021-10-27 21:13:04 -0300

    added componets coin

--- App.js
+++ App.js
@@ -8,9 +8,14 @@
 
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import CoinsStack from './src/components/coins/CoinsStack';
 
 const App = () => {
-  return <NavigationContainer />;
+  return (
+    <NavigationContainer>
+      <CoinsStack />
+    </NavigationContainer>
+  );
 };
 
 export default App;
