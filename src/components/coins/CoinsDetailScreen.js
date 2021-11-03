9bf59a6f3f78b16134e4b52598276d4a5d881ca0
Ezequiel A. Freire <ezequiel.a.freire@gmail.com>
2021-11-03 18:02:14 -0300

    added image in detail

--- src/components/coins/CoinsDetailScreen.js
+++ src/components/coins/CoinsDetailScreen.js
@@ -1,24 +1,46 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
+import Colors from '../../utils/colors';
 
 class CoinsDetailScreen extends Component {
   state = {
     coin: {},
   };
 
+  getSymbolIcon = name => {
+    if (name) {
+      const symbol = name.toLowerCase();
+      return `https://c1.coinlore.com/img/25x25/${symbol}.png`;
+    }
+  };
+
   componentDidMount() {
     const {coin} = this.props.route.params;
     this.setState({coin});
-    this.props.navigation.setOptions({title: coin.symbol});
+    this.props.navigation.setOptions({title: coin.name});
   }
 
   render() {
+    const {coin} = this.state;
     return (
       <View>
-        <Text>Coin Detail Screen</Text>
+        <View>
+          <Image
+            style={styles.iconImage}
+            source={{uri: this.getSymbolIcon(coin.symbol)}}
+          />
+          <Text>{coin.name}</Text>
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  iconImage: {
+    width: 25,
+    height: 25,
+  },
+});
 
 export default CoinsDetailScreen;
