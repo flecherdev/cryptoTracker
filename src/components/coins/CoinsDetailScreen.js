80fbd496e783c78fd7f1640ae092df97dbc71dd3
Ezequiel A. Freire <ezequiel.a.freire@gmail.com>
2021-11-04 12:02:20 -0300

    added data in detail

--- src/components/coins/CoinsDetailScreen.js
+++ src/components/coins/CoinsDetailScreen.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Text, Image, StyleSheet} from 'react-native';
+import {View, Text, Image, StyleSheet, SectionList} from 'react-native';
 import Colors from '../../utils/colors';
 
 class CoinsDetailScreen extends Component {
@@ -9,37 +9,101 @@ class CoinsDetailScreen extends Component {
 
   getSymbolIcon = name => {
     if (name) {
-      const symbol = name.toLowerCase();
+      const symbol = name.toLowerCase().replace(' ', '-');
       return `https://c1.coinlore.com/img/25x25/${symbol}.png`;
     }
+  };
+
+  getSection = coin => {
+    const section = [
+      {
+        title: 'Marcket cap',
+        data: [coin.market_cap_usd],
+      },
+      {
+        title: 'Volume 24h',
+        data: [coin.volume24],
+      },
+      {
+        title: 'Change 24h',
+        data: [coin.percent_change_24h],
+      },
+    ];
+
+    return section;
   };
 
   componentDidMount() {
     const {coin} = this.props.route.params;
     this.setState({coin});
-    this.props.navigation.setOptions({title: coin.name});
+    this.props.navigation.setOptions({title: coin.symbol});
   }
 
   render() {
     const {coin} = this.state;
     return (
-      <View>
-        <View>
+      <View style={styles.container}>
+        <View style={styles.subheader}>
           <Image
             style={styles.iconImage}
-            source={{uri: this.getSymbolIcon(coin.symbol)}}
+            source={{uri: this.getSymbolIcon(coin.name)}}
           />
-          <Text>{coin.name}</Text>
+          <Text style={styles.titleText}>{coin.name}</Text>
         </View>
+        <SectionList
+          sections={this.getSection(coin)}
+          keyExtractor={item => item}
+          renderItem={({item}) => (
+            <View style={styles.sectionItem}>
+              <Text style={styles.itemText}>{item}</Text>
+            </View>
+          )}
+          renderSectionHeader={({section}) => (
+            <View style={styles.sectionHeader}>
+              <Text style={styles.sectionText}>{section.title}</Text>
+            </View>
+          )}
+        />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.charade,
+  },
+  subheader: {
+    backgroundColor: 'rgba(0,0,0, 0.1)',
+    padding: 16,
+    flexDirection: 'row',
+  },
+  titleText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginLeft: 16,
+  },
   iconImage: {
     width: 25,
     height: 25,
+  },
+  sectionHeader: {
+    backgroundColor: 'rgba(0,0,0, 0.1)',
+    padding: 8,
+  },
+  sectionItem: {
+    padding: 8,
+  },
+  itemText: {
+    color: '#fff',
+    fontSize: 14,
+  },
+  sectionText: {
+    color: '#fff',
+    fontSize: 14,
+    fontWeight: 'bold',
   },
 });
 
