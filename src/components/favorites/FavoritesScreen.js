32f15558c0a6607513b70fe706803d79286456dc
Ezequiel A. Freire <ezequiel.a.freire@gmail.com>
2021-11-10 23:35:19 -0300

    [feature] add onpress for list favorite

--- src/components/favorites/FavoritesScreen.js
+++ src/components/favorites/FavoritesScreen.js
@@ -1,13 +1,52 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, FlatList, StyleSheet} from 'react-native';
 import FavoritesEmptyState from './FavoritesEmptyState';
 import Colors from '../../utils/colors';
-
+import Store from '../../utils/storage';
+import {CoinsItem} from '../coins/CoinsItem';
 class FavoritesScreen extends Component {
+  state = {
+    favorites: [],
+  };
+
+  getFavorite = async () => {
+    try {
+      const allKeys = await Store.instance.getAllKeys();
+      const keys = allKeys.filter(key => key.includes('favorite-'));
+      const favs = await Store.instance.getAll(keys);
+      const favorites = favs.map(fav => JSON.parse(fav[1]));
+      console.log('favs', favorites);
+      this.setState({favorites: favorites});
+    } catch (error) {
+      console.log(`get favorite error ${error}`);
+    }
+  };
+
+  handlerPress = coin => {
+    this.props.navigation.navigate('Detail', {coin});
+  };
+
+  componentDidMount() {
+    this.props.navigation.addListener('focus', this.getFavorite);
+  }
+
+  componentWillUnmount() {
+    this.props.navigation.removeListener('focus', this.getFavorite);
+  }
+
   render() {
+    const {favorites} = this.state;
     return (
       <View style={styles.container}>
-        <FavoritesEmptyState />
+        {favorites.length === 0 ? <FavoritesEmptyState /> : null}
+        {favorites.length > 0 ? (
+          <FlatList
+            data={favorites}
+            renderItem={({item}) => (
+              <CoinsItem item={item} onPress={() => this.handlerPress(item)} />
+            )}
+          />
+        ) : null}
       </View>
     );
   }
